d66aeff6d4205f86bf142d4421770838fa583ae5
Teemu Matvejeff <tamndrei567@gmail.com>
2015-09-22 17:08:31 +0300

    First commit of BibTex-project.

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author teemu
+ */
+public class Book extends Reference{
+    
+    private int year;
+    private String author;
+    private String title;
+    private String publisher;
+    
+    public Book(int year, String author, String title,
+                String publisher) {
+        this.year = year;
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+    }
+    
+    @Override
+    public void print() {
+        
+        System.out.println("@book{");
+    }
+}
---
+++ src/Container.java
@@ -0,0 +1,36 @@
+
+import java.util.Hashtable;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author teemu
+ */
+public class Container {
+    
+    private Hashtable<String, Reference> references;
+    
+    public Container() {
+        
+    }
+    
+    // Add reference to reference list
+    public void addReference(Reference r) {
+        this.references.put(r.getId(), r);
+    }
+    
+    public void deleteReference(Reference r) {
+        this.references.remove(r);
+    }
+    
+    public List<Reference> listReferences() {
+        
+        return null;
+    }
+}
