79d02a25f78e2bda30a028bebd488ff9fd4d7d48
Lutikka <erkki.sinisalo@gmail.com>
2015-09-28 14:06:16 +0300

    UI input check

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -14,31 +14,31 @@ import java.util.Scanner;
  * @author teemu
  */
 public class UserInterface {
-    
+
     // User interface container object.
     Container container;
-    
+
     // Scanner object for reading input.
     Scanner scanner;
-    
+
     public UserInterface() {
         container = new Container();
         scanner = new Scanner(System.in);
     }
-    
+
     public Container getContainer() {
         return this.container;
     }
-    
-    public void start() {        
+
+    public void start() {
         System.out.println("MAIN MENU \n");
         System.out.println("You can perform following actions. "
                 + "Press the key in the wave-bracket:");
         System.out.println("- Add a book reference (B) \n");
         System.out.println("- List existing references (L) \n");
-        
-        String answer = this.scanner.next();
-        
+
+        String answer = this.scanner.nextLine();
+
         if (answer.equals("B")) {
             addBookReference();
         } else if (answer.equals("L")) {
@@ -49,40 +49,64 @@ public class UserInterface {
             System.exit(0);
         }
     }
-    
+
     // Method for adding book reference.
     public void addBookReference() {
         Book book = new Book();
-        
+        String regex;
         System.out.println("Input mandatory fields for book referece \n");
 //        System.out.println("If you want to stop book creation, write 'halt' \n\n");
-        System.out.println("Give year book is written: \n");
-        
+
         // Add year to the book.
-        Integer year = this.scanner.nextInt();
-        book.setYear(year);
-        
+        regex = "[0-9]+";
+        while (true) {
+            System.out.println("Give the year in which the book was published: \n");
+            String year = this.scanner.nextLine();
+            if (year.matches(regex)) {
+                book.setYear(Integer.parseInt(year));
+                break;
+            }
+            System.out.println("Invalid input");
+        }
+
         // Add author
-        System.out.println("Give book author:\n");
-        String author = this.scanner.next();
-        book.setAuthor(author);
-        
+        regex = "([a-zA-Z]+[\\s]*)+";
+        while (true) {
+            System.out.println("Give book author:\n");
+            String author = this.scanner.nextLine();
+            if (author.matches(regex)) {
+                book.setAuthor(author);
+                break;
+            }
+            System.out.println("Invalid input");
+
+        }
         // Add title
-        System.out.println("Give book title:\n");
-        String title = this.scanner.next();
-        book.setTitle(title);
-        
+        while (true) {
+            System.out.println("Give book title:\n");
+            String title = this.scanner.nextLine();
+            if (title.matches(regex)) {
+                book.setTitle(title);
+                break;
+            }
+            System.out.println("Invalid input");
+        }
         // Add publisher
-        System.out.println("Give book publisher:\n");
-        String publisher = this.scanner.next();
-        book.setPublisher(publisher);
-        
+        while (true) {
+            System.out.println("Give book publisher:\n");
+            String publisher = this.scanner.nextLine();
+            if (publisher.matches(regex)) {
+                book.setPublisher(publisher);
+                break;
+            }
+            System.out.println("Invalid input");
+        }
         container.addReference(book);
-        
+
         System.out.println("new book reference has been created!");
-        System.out.println("There are now " + this.container.getReferences().size() + 
-                " references in the system.");
-        
+        System.out.println("There are now " + this.container.getReferences().size()
+                + " references in the system.");
+
         start();
     }
 }
