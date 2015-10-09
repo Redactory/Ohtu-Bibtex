2baf7a24198860b6eeb0a1c74c2e4be560d22f38
Lutikka <erkki.sinisalo@gmail.com>
2015-10-09 19:49:37 +0300

    Persistence by file

--- src/main/java/ohtu/bibtex/OhtuBibTex.java
+++ src/main/java/ohtu/bibtex/OhtuBibTex.java
@@ -5,11 +5,13 @@
  */
 package main.java.ohtu.bibtex;
 
-import main.java.Models.Book;
+import java.io.File;
+import java.util.List;
+import main.java.IO.IO;
 import main.java.Models.Container;
+import main.java.Models.Reference;
+import main.java.Models.ReferenceConverter;
 import main.java.UI.UserInterface;
-import main.java.Models.Article;
-import main.java.Models.Inproceeding;
 
 
 /**
@@ -21,37 +23,41 @@ public class OhtuBibTex {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {     
         UserInterface userInterface = new UserInterface();
         
         Container container = userInterface.getContainer();
-
-        // Adding some book references.
-        Book book1 = new Book(1997, "Jennifer Lowett", "I Burn For You", "Harlequin");
-        Book book2 = new Book(1889, "Don Rosa", "Life and Times of Scrooge McDuck", "Nothington");
-        Book book3 = new Book(2000, "Steve Hackwell", "Fount east of Altaria", "Baen");
-        book3.setNumber("2");
-        container.addReference(new Book(1896,"Jaakko","Jaakon selitykset","ACM"));
-        container.addReference(new Book(2001,"Martti Laaksonen","Taide","DELTA"));
-        Article a = new Article();
-        a.setAuthor("Tapio Kunnas");
-        a.setTitle("Ne laulut");
-        a.setYear(1995);
-        a.setJournal("Juu");
-        a.setPages("2");
-        
-        Inproceeding i = new Inproceeding();
-        i.setAuthor("Mikko Kuningas");
-        i.setTitle("Night coding");
-        i.setBooktitle("Progz");
-        i.setYear(2012);
-        i.setNote("note1");
-        container.addReference(i);
-        container.addReference(a);
-        
-        container.addReference(book1);
-        container.addReference(book2);
-        container.addReference(book3);
+        //Adding some book references
+        String string = IO.readBibTexFile(new File("bib_data.bib"));
+        List<Reference> refs = ReferenceConverter.bibTexToReference(string);
+        for (Reference ref : refs) {
+            container.addReference(ref);
+        }
+//        Book book1 = new Book(1997, "Jennifer Lowett", "I Burn For You", "Harlequin");
+//        Book book2 = new Book(1889, "Don Rosa", "Life and Times of Scrooge McDuck", "Nothington");
+//        Book book3 = new Book(2000, "Steve Hackwell", "Fount east of Altaria", "Baen");
+//        book3.setNumber("2");
+//        container.addReference(new Book(1896,"Jaakko","Jaakon selitykset","ACM"));
+//        container.addReference(new Book(2001,"Martti Laaksonen","Taide","DELTA"));
+//        Article a = new Article();
+//        a.setAuthor("Tapio Kunnas");
+//        a.setTitle("Ne laulut");
+//        a.setYear(1995);
+//        a.setJournal("Juu");
+//        a.setPages("2");
+//        
+//        Inproceeding i = new Inproceeding();
+//        i.setAuthor("Mikko Kuningas");
+//        i.setTitle("Night coding");
+//        i.setBooktitle("Progz");
+//        i.setYear(2012);
+//        i.setNote("note1");
+//        container.addReference(i);
+//        container.addReference(a);
+//        
+//        container.addReference(book1);
+//        container.addReference(book2);
+//        container.addReference(book3);
 //        userInterface.getContainer().listReferences();
         userInterface.start();    
     }
