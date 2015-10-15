1f32993d3a915259532a1ca0616c54660c4489a9
Teemu Matvejeff <tamndrei567@gmail.com>
2015-10-15 17:26:06 +0300

    Masterin täyttäminen.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ohtu-Bibtex
+
+Trello:
+https://trello.com/b/pB6lKuI7/miniprojekti
+
+Burndown chart:
+https://BurndownForTrello.com/share/6bhmca5djr
+
+Jenkins:
+http://ohtu.jamo.io/job/miniprojekti-riot2/
+
+Tuntikirjaus:
+https://docs.google.com/spreadsheets/d/1kSf5dMOyBZSLS8bE4vKogMRIqWWO6Xg5EK4hGSR5MAE/edit?usp=sharing
+
+Loppuraportti:
+https://docs.google.com/document/d/1tKxi0SjCsmOXgAWQmJgrch5jNpfo-xfcejNpmsV_RmM/edit?usp=sharing
+
+Riot-E!-ryhmän jäsenet ovat:
+
+- Teemu Matvejeff
+- Heli Niemimaa
+- Erkki Sinisalo
+- Lari Saukkonen
---
+++ src/main/java/Models/Container.java
@@ -0,0 +1,50 @@
+package Models;
+
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author teemu
+ */
+public class Container {
+    
+    private Hashtable<String, Reference> references;
+    
+    public Container() {
+        this.references = new Hashtable<String, Reference>();
+    }
+    
+    // Getter method for getting references-list.
+    public Hashtable getReferences() {
+        return references;
+    }
+    
+    // Add reference to reference to database.
+    public void addReference(Reference r) {
+        references.put(r.getId(), r);
+    }
+    
+    // Remove wanted reference from database.
+    public void deleteReference(Reference r) {
+        this.references.remove(r.getId());
+    }
+    
+    // Method for listing references in database.
+    public List<Reference> listReferences() {
+        List<Reference> l = new ArrayList<Reference>();
+        for (String k : references.keySet()) {
+            Reference r = references.get(k);
+            l.add(r);
+        }
+        return l;
+    }
+}
